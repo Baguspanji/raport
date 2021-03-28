a4f18aee0bdca491ffb6d613aec922ef44391ad1
Baguspanji <pitungs060@gmail.com>
2021-03-29 03:09:41 +0700

    auth

--- application/helpers/allowed_helper.php
+++ application/helpers/allowed_helper.php
@@ -3,15 +3,23 @@ function allowed($param, $param2 = null)
 {
 	$CI = &get_instance();
 	$role = $CI->session->userdata('role');
-	if ($role == $param || $role == $param2) {
-	} else {
-		if ($param == 'admin') {
-			$CI->session->set_flashdata('notifikasi', '<script>notifikasi("Anda Harus Login Terlebih Dahulu", "danger", "fa fa-exclamation")</script>');
+	if ($role == null) {
+		if ($param != null && $param2 != null) {
+			$CI->session->set_flashdata('notifikasi', '<script>notifikasi("Anda harus login terlebih dahulu", "danger", "fa fa-exclamation")</script>');
 			redirect(base_url('admin/login'));
 		} else {
-			$CI->session->set_flashdata('notifikasi', '<script>notifikasi("Anda Harus Login Sebagai Pelanggan", "danger", "fa fa-exclamation")</script>');
-
-			redirect(base_url());
+			if ($param == 'admin') {
+				$CI->session->set_flashdata('notifikasi', '<script>notifikasi("Anda Harus Login Sebagai Admin", "danger", "fa fa-exclamation")</script>');
+				redirect(base_url('admin/login'));
+			} else {
+				$CI->session->set_flashdata('notifikasi', '<script>notifikasi("Anda Harus Login", "danger", "fa fa-exclamation")</script>');
+				redirect(base_url());
+			}
+		}
+	}elseif ($role != $param && $param2 == null) {
+		if ($param == 'admin') {
+			$CI->session->set_flashdata('notifikasi', '<script>notifikasi("Anda Harus Login Sebagai Admin", "danger", "fa fa-exclamation")</script>');
+			redirect(base_url('admin/login'));
 		}
 	}
 }
